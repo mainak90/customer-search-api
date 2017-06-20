77d2dd2853582e8ddfde2cfacdea7652b835fac6
Oleg Korolenko <oleg.korolenko.ext@proximus.com>
2017-06-20 16:58:27 +0200

    put /user  to a separated route file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const userRoute = require('./routes/user')
 
 const app = express()
 const port = process.env.PORT || 3000
-async function getUserAsync (username) {
-  const res = await Promise.resolve(`${username} is a user !`)
-  return {user: await res, found: res.status === 200}
-}
 
-app.get('/user', async (req, res) => {
-  try {
-    // const {username} = req.params;
-    const userResult = await getUserAsync('John')
-    res.send(userResult)
-  } catch (e) {
-    res.status(500).end()
-  }
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/user', userRoute)
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
