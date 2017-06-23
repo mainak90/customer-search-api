b3ca9feb5bc883b2c95b7c58d6af82b5809dd5be
Oleg Korolenko <oleg.korolenko.ext@proximus.com>
2017-06-23 17:09:10 +0200

    added clustering with as many forks as cpu cores

--- src/app.js
+++ src/app.js
@@ -4,19 +4,33 @@ const bodyParser = require('body-parser')
 const validationRoute = require('./routes/validation')
 const customersRoute = require('./routes/customers')
 const validator = require('express-validator')
-const app = express()
 const config = require('../config')
 const port = process.env.PORT || config.port
 const logger = require('./logger')
 const morgan = require('morgan')
+const cluster = require('cluster')
 
-app.use(helmet())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(validator())
-app.use(morgan('combined', { stream: logger.stream }))
+if (cluster.isMaster) {
+  const cpuCount = require('os').cpus().length
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker) => {
+    logger.warn('Worker %d died :(', worker.id)
+    cluster.fork()
+  })
+} else {
+  const app = express()
+  app.use(helmet())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(validator())
+  app.use(morgan('combined', {stream: logger.stream}))
 
-app.use(validationRoute)
-app.use('/customers', customersRoute)
+  app.use(validationRoute)
+  app.use('/customers', customersRoute)
+  app.listen(port, () => logger.info(`customer-search app started on port ${port}!`))
+}
 
-app.listen(port, () => logger.info(`customer-search app started on port ${port}!`))
+
+
