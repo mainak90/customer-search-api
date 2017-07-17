527b58ff61e8be601850a3e1b0409a66f5b69a38
Oleg Korolenko <oleg.korolenko.ext@proximus.com>
2017-07-17 13:59:35 +0200

    (feat) added customer search by msisdn

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,31 +1,46 @@
 const express = require('express')
 const config = require('../../config')
 const customerService = require('../services/cdb-customer.service')
+const installbaseService = require('../services/installbase.service')
 const commonResponses = require('../common/response')
+const _ = require('lodash')
 
 const customersRoute = (validation) => {
   const router = express.Router()
   router.get('/', validation.validateQuery, async (req, res) => {
     try {
-      const result = await
-        customerService.search(config, req.query)
-      const response = {message: [{searchCriteria: 'byName', result: result}]}
-      res.send(response)
+      const result = await customerService.search(config, req.query)
+      res.send({message: [{searchCriteria: 'byName', result: result}]})
     } catch (e) {
       commonResponses.serverError(res, e)
     }
   })
 
-  router.get('/accessNumber/:accessNumber', validation.validateReq, async (req, res) => {
+  router.get('/accessNumber/:accessNumber', validation.validateAccessNumber, async (req, res) => {
     try {
-      const result = await
-        customerService.searchByAccessNumber(config, req.params)
-      const response = {message: [{searchCriteria: 'byAccessNumber', result: result}]}
-      res.send(response)
+      const result = await customerService.searchByAccessNumber(config, req.params)
+      res.send({message: [{searchCriteria: 'byAccessNumber', result: result}]})
     } catch (e) {
       commonResponses.serverError(res, e)
     }
   })
+
+  router.get('/msisdn/:msisdn', validation.validateMsisdn, async (req, res) => {
+    try {
+      const subs = await installbaseService.search(config, req.params)
+      if (_.isArray(subs.mobileSubscriptions) &&
+        subs.mobileSubscriptions.length > 0 &&
+        subs.mobileSubscriptions[0].pniAccount) {
+        const result = await customerService.searchByPni(config, subs.mobileSubscriptions[0].pniAccount)
+        res.send({message: [{searchCriteria: 'byMsisdn', result: result}]})
+      } else {
+        res.send({message: [{searchCriteria: 'byMsisdn', result: []}]})
+      }
+    } catch (e) {
+      commonResponses.serverError(res, e)
+    }
+  })
+
   return router
 }
 
